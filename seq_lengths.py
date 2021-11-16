54a26bcab6d7d376f82937ec71895f19b7451275
Gina Knowles <gaknowle@login-5.saga>
2021-11-16 14:48:13 +0100

    hei

--- seq_lengths.py
+++ seq_lengths.py
@@ -3,4 +3,10 @@ with open(sys.argv[1],"r") as fh:
     lines=fh.readlines()
 for line in lines:
     line=line.strip()
+    if line.startswith(">"):
+   
+       header=line
+    else:
+        length=len(line)
+        print(length,header)
     print(len(line))
